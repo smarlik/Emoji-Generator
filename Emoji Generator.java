9a627751d0d9e54292436a8110c2627eccd38e15
smarlik <49741150+smarlik@users.noreply.github.com>
2021-06-05 13:43:21 -0700

    Add files via upload

---
+++ Emoji Generator.java
@@ -0,0 +1,14 @@
+
+printf("How are you feeling today? \n 1) Happy \n 2) Grumpy \n 3) Sad \n 4) calm \n");
+int n=readInt();
+switch(n) {
+	case 1: {printf(" Your Emoji: \n \n :D"); break;}
+
+	case 2: {printf(" Your Emoji: \n \n  >:["); break;}
+
+	case 3: {printf(" Your Emoji: \n \n  :'("); break;}
+
+	case 4: {printf(" Your Emoji: \n \n  :3");break;}
+}
+printf("\n\n\n\n ");
+read();
