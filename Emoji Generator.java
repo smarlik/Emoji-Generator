04d1cb8faf4f18b62445f14186b3c8ddf5581ae7
smarlik <smarlik@protonmail.com>
2025-08-02 16:54:21 -0700

    fixed emoji generator issues

---
+++ Emoji Generator.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+String read() {
+    try {
+        return new Scanner(System.in).nextLine();
+    } catch (Exception e) {
+        e.printStackTrace();
+        return "";
+    }
+}
+
+int readInt() {
+    try {
+        return new Scanner(System.in).nextInt();
+    } catch (Exception e) { 
+        e.printStackTrace();
+        return 0;
+    }
+}
+
+printf("How are you feeling today? \n 1) Happy \n 2) Grumpy \n 3) Sad \n 4) calm \n");
+int n=readInt();
+switch(n) {
+	case 1: {printf(" Your Emoji: \n \n :D"); break;}
+
+	case 2: {printf(" Your Emoji: \n \n  >:["); break;}
+
+	case 3: {printf(" Your Emoji: \n \n  :'("); break;}
+
+	case 4: {printf(" Your Emoji: \n \n  :|");break;}
+}
+printf("\n\n\n\n Press enter to exit");
+read();
+System.exit(0);
